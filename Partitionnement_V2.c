39237f4abb72bd37a634443f617afce2c1b351cd
Aida SOW <aidasow@esp.sn>
2026-07-09 22:06:10 +0000

    Another changes

--- Partitionnement_V2.c
+++ Partitionnement_V2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/* Echange les valeurs situees aux positions i et j dans le tableau. */
 void Permuter (int tab[], int i, int j) {
     int temp;
     temp = tab[i];
@@ -7,21 +8,34 @@ void Permuter (int tab[], int i, int j) {
     tab[j] = temp;
 }
 
+/* Partitionne le tableau entre les indices i et j autour du pivot tab[i]. */
 int PartitionnementV2 (int tab[], int i, int j) {
+    /* l parcourt le tableau depuis la gauche, k depuis la droite. */
     int l = i + 1, k = j;
+
+    /* On cherche les elements mal places par rapport au pivot. */
     while (l <= k) {
+        /* Deplacement de k vers la gauche tant que l'element est superieur au pivot. */
         while (tab[i] < tab[k]) {
             k = k - 1;
         }
+
+        /* Deplacement de l vers la droite tant que l'element est inferieur ou egal au pivot. */
         while (tab[i] >= tab[l]) {
             l = l + 1;
         }
+
+        /* Si les deux indices ne se sont pas croises, on echange les elements mal places. */
         if (l < k) {
             Permuter (tab, l, k);
             k--;
             l++;
         }
     }
+
+    /* Placement du pivot a sa position definitive dans la partition. */
     Permuter (tab, i, k);
+
+    /* Retourne l'indice final du pivot. */
     return k;
 }
--- Tri_a_bulle.c
+++ Tri_a_bulle.c
@@ -2,10 +2,15 @@
 #include <stdlib.h> /*C'est la bibliotheque contenant les fonctions srand et rand*/
 #include <time.h> /*bibliotheque contenant les outils neccessaires pour mesurer le temps d'execution*/
 
+/* Procedure qui trie le tableau T de taille n avec l'algorithme du tri a bulle. */
 void TriBulle (int n, int T[]) {
     int i, j, temp;
+
+    /* A chaque passage, le plus grand element restant remonte vers la fin. */
     for (i = n - 1; i >= 1; i--) {
+        /* Comparaison des elements voisins jusqu'a la partie deja triee. */
         for (j = 0; j < i; j++) {
+            /* Si deux elements voisins sont dans le mauvais ordre, on les echange. */
             if (T[j] > T[j + 1]) {
                 temp = T[j];
                 T[j] = T[j + 1];
@@ -13,22 +18,35 @@ void TriBulle (int n, int T[]) {
             }
         }
     }
-} /*Procedure du tri a bulle*/
+}
 
 int main () {
+    /* Initialisation du generateur de nombres aleatoires avec l'heure actuelle. */
     srand (time(NULL));
+
     int n;
+
+    /* Lecture de la taille du tableau a trier. */
     printf ("Taille du tableau : ");
     scanf ("%d", &n);
+
     int tab[n], i;
     clock_t debut, fin;
     double temps;
+
+    /* Remplissage du tableau avec des valeurs aleatoires entre 0 et n - 1. */
     for (i = 0; i < n; i++) 
         tab [i] = rand() % n;
+
+    /* Mesure du temps pris uniquement par le tri. */
     debut = clock();
     TriBulle (n, tab);
     fin = clock();
+
+    /* Conversion du temps processeur en secondes. */
     temps = (double)(fin - debut) / CLOCKS_PER_SEC;
+
+    /* Affichage du temps d'execution du tri. */
     printf("Temps d'execution : %f secondes\n", temps);
     return 0;
 }
